4d870c01f1f2ec6bb2f1f1f6ae90f0eb6820eef4
jcleston <janes_cleston@hotmail.com>
2022-11-01 18:50:27 -0300

    feat: add extencion prettier + config vscode

--- src/app/components/fist-component/fist-component.component.ts
+++ src/app/components/fist-component/fist-component.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fist-component',
-  templateUrl: './fist-component.component.html',
-  styleUrls: ['./fist-component.component.css']
+    selector: 'app-fist-component',
+    templateUrl: './fist-component.component.html',
+    styleUrls: ['./fist-component.component.css'],
 })
 export class FistComponentComponent implements OnInit {
+    // Adicionando uma propriedade na classe
+    name: string = 'Cleston';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
