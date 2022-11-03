15866d6aeef4b274aae960e187a27252b7f409fa
jcleston <janes_cleston@hotmail.com>
2022-11-03 18:44:33 -0300

    feat: add new component directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FistComponentComponent } from './components/fist-component/fist-component.component';
 import { SecondComponentComponent } from './components/second-component/second-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FistComponentComponent,
     SecondComponentComponent,
-    ParentDataComponent
+    ParentDataComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule
