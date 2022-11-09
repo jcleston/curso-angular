3f87cf1d4e97d3cb2b6c18fdb152e71670984fba
jcleston <janes_cleston@hotmail.com>
2022-11-09 20:11:15 -0300

    feat: add new component item-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FistComponentComponent } from './components/fist-component/fist-component.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 
 const routes: Routes = [
     { path: '', component: FistComponentComponent },
     { path: 'list', component: ListRenderComponent },
+    { path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
