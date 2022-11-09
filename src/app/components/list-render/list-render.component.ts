cc68510c807aa13c0c9abb36e05c6223f3caf3e0
jcleston <janes_cleston@hotmail.com>
2022-11-09 19:30:26 -0300

    refactor: update list-render + list.service

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -10,16 +10,13 @@ import { ListService } from 'src/app/services/list.service';
     styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-    animals: Animal[] = [
-        { name: 'Ana', type: 'Dog', age: 10 },
-        { name: 'João', type: 'Cat', age: 3 },
-        { name: 'Pedro', type: 'Dog', age: 5 },
-        { name: 'Maria', type: 'Cat', age: 6 },
-    ];
+    animals: Animal[] = [];
 
     animalDetails = '';
 
-    constructor(private listService: ListService) {}
+    constructor(private listService: ListService) {
+        this.getAnimals();
+    }
 
     ngOnInit(): void {}
 
@@ -30,5 +27,11 @@ export class ListRenderComponent implements OnInit {
     removeAnimal(animal: Animal) {
         console.log('Removendo registro...');
         this.animals = this.listService.remove(this.animals, animal);
+    }
+
+    getAnimals(): void {
+        this.listService
+            .getAll()
+            .subscribe(animals => (this.animals = animals));
     }
 }
